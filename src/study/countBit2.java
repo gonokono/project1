9bfe31290990646f798cadbab48465f859928548
eunho.ko <koeh0802@gmail.com>
2023-04-21 15:05:54 +0900

    This is secend commit messages

---
+++ src/study/countBit2.java
@@ -0,0 +1,37 @@
+package study;
+
+public class countBit2 {
+	public static void main(String[] args) {
+		int cnt = countBits(5);
+		System.out.println("Git Test!");
+	}
+
+	public static int countBits(int number) {
+		int num = number;
+		int i = 0;
+
+		while (num > 0) {
+			num /= 2;
+			i++;
+		}
+
+		int[] arr = new int[i];
+		int j = 0;
+		num = number;
+		while (num > 0) {
+			arr[j] = num % 2;
+			num /= 2;
+			j++;
+		}
+
+		int cnt = 0;
+
+		for (int k = 0; k < arr.length; k++) {
+			if (arr[k] == 1) {
+				cnt++;
+			}
+		}
+
+		return cnt;
+	}
+}
